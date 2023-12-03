4e2e615cf7441bd61a4d58883ecef127dad2a2e1
Rahul <bjrajput15800@gmail.com>
2023-12-03 19:17:26 +0530

    add movies api

--- super-app/src/Pages/EntertainmentPage.jsx
+++ super-app/src/Pages/EntertainmentPage.jsx
@@ -1,10 +1,56 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import '../Components/Entertainment/EntertainmentPage.css'
+import axios from 'axios';
 
 const EntertainmentPage = () => {
-  return (
-    <div className='entertainment-page'>EntertainmentPage</div>
-  )
+    const selectedCategories = JSON.parse(localStorage.getItem('selectedCategories'));
+    
+    const apiKey = 'da6b8268';
+    const baseApiUrl = 'https://www.omdbapi.com/';
+    const [moviesData, setMoviesData] = useState({});
+
+    const fetchMoviesByGenre = async (genre) => {
+        try {
+            const response = await axios.get(`${baseApiUrl}?apikey=${apiKey}&s=${genre}`);
+            return response.data.Search || [];
+        } catch (error) {
+            return console.error('Error fetching movies:', error);
+        }
+    };
+
+    const fetchMoviesForSelectedGenres = async () => {
+        const genreMovies = {};
+        for (const genre of selectedCategories) {
+            const movies = await fetchMoviesByGenre(genre);
+            genreMovies[genre] = movies;
+        }
+        setMoviesData(genreMovies);
+    };
+
+    useEffect(() => {
+        fetchMoviesForSelectedGenres();
+    }, [selectedCategories]);
+
+    return (
+        <div className='entertainment-page'>
+            <div className="page-heading">Entertainment according to your choice</div>
+            <div className="browse-categories">
+            {Object.keys(moviesData).map((genre) => (
+                        <div className="browsed-category" key={genre}>
+                            <div className="category-name">{genre}</div>
+                            <div className="category-content">
+                            {moviesData[genre].map((movie, index) => (
+                                <div key={index} className="movie-poster">
+                                <img id='movie-poster' src={movie.Poster}/>
+                                </div>
+                            ))}
+                            </div>
+                        </div>
+                    ))
+                }
+            </div>
+        </div>
+    )
 }
 
 export default EntertainmentPage;
